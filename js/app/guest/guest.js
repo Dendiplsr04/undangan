58475b9fa1813787b39406051e52428bf5940e05
Dendiplsr04 <dendipalasara455@gmail.com>
2026-04-26 18:20:02 +0700

    fix: update calendar button with correct names and date

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -255,10 +255,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Zahra and Aang',
+            dates: `${formatDate('2026-05-22 09:00')}/${formatDate('2026-05-22 12:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'Kp. Rasamala RT 002/RW 002 Ds. Cibadak Kec. Sukamakmur Kab. Bogor',
             ctz: config.get('tz'),
         });
 
